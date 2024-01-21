d22d5df224f201d41451e46e4c93f53f98866b09
Egor Gluhih <eagluhih@gmail.com>
2024-01-21 13:24:44 +0300

    Time: 135 ms (82.09%), Space: 17.9 MB (66.33%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,4 +1,15 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        nums.sort()
-        return nums[len(nums) // 2]
+        counter = 0
+        cantidate = 0
+
+        for num in nums:
+            if counter == 0:
+                candidate = num
+            
+            if candidate == num:
+                counter += 1
+            else:
+                counter -= 1
+        
+        return candidate
