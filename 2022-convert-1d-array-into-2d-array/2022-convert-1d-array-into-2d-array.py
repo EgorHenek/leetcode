efe4ba2209632020c996fd268f34587192d4b6ef
Egor Gluhih <eagluhih@gmail.com>
2024-01-21 13:52:12 +0300

    Time: 710 ms (68.15%), Space: 24.2 MB (76.95%) - LeetHub

--- 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.py
+++ 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.py
@@ -1,10 +1,9 @@
 class Solution:
     def construct2DArray(self, original: List[int], m: int, n: int) -> List[List[int]]:
         if m * n == len(original):
-            result = []
-            offset = 0
-            while offset < len(original):
-                result.extend([original[offset:offset+n]])
-                offset += n
-            return result
+            row = 0
+            while row < m:
+                original[row:row+n] = [original[row:row+n]]
+                row += 1
+            return original
         return []
