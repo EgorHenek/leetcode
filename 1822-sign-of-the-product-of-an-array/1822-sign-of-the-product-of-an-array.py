255749e036ef045ba8122d361388b9780cd89735
Egor Gluhih <eagluhih@gmail.com>
2024-02-03 17:46:21 +0300

    Time: 54 ms (87.57%), Space: 16.7 MB (73.93%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.py
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.py
@@ -1,8 +1,9 @@
 class Solution:
     def arraySign(self, nums: List[int]) -> int:
-        multi = reduce(lambda v,x:  v * x, nums)
-        if multi > 0:
-            return 1
-        elif multi == 0:
-            return 0
-        return -1
+        result = 1
+        for i in nums:
+            if i == 0:
+                return 0
+            elif i < 0:
+                result *= -1
+        return result
