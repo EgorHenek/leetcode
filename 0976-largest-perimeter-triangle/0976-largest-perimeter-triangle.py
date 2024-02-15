8f9a006c0d754f0c5d6795c1aef2ba3d1ab50a2c
Egor Gluhih <eagluhih@gmail.com>
2024-02-15 11:11:47 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.py
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.py
@@ -4,6 +4,7 @@ class Solution:
         ans = 0
         for i in range(2, len(nums)):
             a, b, c = (nums[d] for d in range(i - 2, i + 1))
-            if a + b > c:
+            if a + b > c and a + c > b and b + c > a:
                 ans = a + b +c
+                return ans
         return ans
