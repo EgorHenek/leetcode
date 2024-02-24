306004ff7aa25731004f032602f28a19f7e6109a
Egor Gluhih <eagluhih@gmail.com>
2024-02-24 10:26:47 +0300

    Time: 34 ms (72.84%), Space: 16.5 MB (68.89%) - LeetHub

--- 1844-replace-all-digits-with-characters/1844-replace-all-digits-with-characters.py
+++ 1844-replace-all-digits-with-characters/1844-replace-all-digits-with-characters.py
@@ -1,11 +1,9 @@
 class Solution:
     def replaceDigits(self, s: str) -> str:
-        letters = string.ascii_lowercase
         result = []
         for i in range(1, len(s), 2):
-            letter_index = letters.index(s[i-1])
             result.append(s[i-1])
-            result.append(letters[letter_index + int(s[i])])
+            result.append(chr(ord(s[i-1]) + int(s[i])))
         if len(s) % 2:
             result.append(s[-1])
         return "".join(result)
